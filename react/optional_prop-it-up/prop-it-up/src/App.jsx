c73fc0e072f8c6bd08004e1b62de6470d9592cbe
Benji Hix <benji.hix@outlook.com>
2023-09-26 18:04:21 -0700

    props/components basics

---
+++ react/optional_prop-it-up/prop-it-up/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import PersonCard from './components/PersonCard'
+
+function App() {
+
+  return (
+    <>
+    <PersonCard firstName={'Jane'} lastName={'Doe'} age={45} hairColor={'black'} />
+    <PersonCard firstName={'Smith'} lastName={'John'} age={88} hairColor={'brown'} />
+    <PersonCard firstName={'Fillmore'} lastName={'Millard'} age={50} hairColor={'brown'} />
+    <PersonCard firstName={'Smith'} lastName={'Maria'} age={62} hairColor={'brown'} />
+    </>
+  );
+}
+
+export default App
